36b333e2e7d1804154259d7e43d24b1acfd71256
aakashdeveloper <ahanda205@gmail.com>
2021-10-08 10:18:16 +0530

    expressRouter

---
+++ fullstack/src/router/categoryRouter.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var categoryRouter = express.Router();
+
+var category = [
+    {
+        "id":1,
+        "category": "Fashion",
+        "thumb":"https://i.ibb.co/56VP0Fn/cloths.jpg"
+    },
+    {
+        "id":2,
+        "category":"Electronics",
+        "thumb":"https://i.ibb.co/pw5Wtdx/appliances.jpg"
+    },
+    {
+        "id":3,
+        "category":"Essentials",
+        "thumb":"https://i.ibb.co/0cw34xm/essentials.jpg"
+    },
+    {
+        "id":4,
+        "category": "Footwear",
+        "thumb":"https://i.ibb.co/r3SZq8S/footware.jpg"
+    }
+]
+
+categoryRouter.route('/')
+    .get((req,res) => {
+        res.send(category)
+    })
+
+categoryRouter.route('/details')
+    .get((req,res) => {
+        res.send('category details')
+    })
+
+module.exports = categoryRouter
