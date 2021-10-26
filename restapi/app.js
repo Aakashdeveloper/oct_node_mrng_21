c21d294bfbc29705d70e7a6ab9ca99e86a7f49b6
aakashdeveloper <ahanda205@gmail.com>
2021-10-26 10:18:59 +0530

    restapicomplete

--- restapi/app.js
+++ restapi/app.js
@@ -8,8 +8,16 @@ const MongoClient = mongo.MongoClient;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoUrl = process.env.MongoUrl;
+const swaggerUi = require('swagger-ui-express');
+const package = require('./package.json');
+const swaggerDocument = require('./swagger.json');
+
+swaggerDocument.info.version = package.version;
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 let db;
 let col_name = "octUser";
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -42,11 +50,20 @@ app.get('/users', (req,res) => {
         query = {isActive:isActive}
     }
     else{
-        query = {}
+        query = {isActive:true}
     }
     db.collection(col_name).find(query).toArray((err,data) => {
         if(err) throw err;
         res.status(200).send(data)
+    })
+})
+
+//find particular user
+app.get('/user/:id',(req,res) => {
+    var id = mongo.ObjectId(req.params.id);
+    db.collection(col_name).find({_id:id}).toArray((err,result) => {
+        if(err) throw err;
+        res.status(200).send(result)
     })
 })
 
@@ -57,6 +74,71 @@ app.post('/addUser',(req,res) => {
         if(err) throw err;
         res.status(200).send('data added')
     })
+})
+
+// updateUser
+app.put('/updateUser',(req,res) => {
+    db.collection(col_name).updateOne(
+        {_id:mongo.ObjectId(req.body._id)},
+        {
+            $set:{
+                name: req.body.name,
+                city: req.body.city,
+                phone: req.body.phone,
+                role: req.body.role,
+                isActive: true,
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.status(200).send('data updated')
+        }
+    )
+})
+
+//delete user
+app.delete('/deleteUser', (req,res) => {
+    db.collection(col_name).remove(
+        {_id:mongo.ObjectId(req.body._id)},(err,result) => {
+            if(err) throw err;
+            res.send("Data Deleted")
+        }
+    )
+})
+
+// deactivate
+app.put('/deactivate', (req,res) => {
+    db.collection(col_name).updateOne(
+        {_id:mongo.ObjectId(req.body._id)},
+        {
+            $set:{
+                isActive:false
+            }
+        },(err,result)=>{
+            if(err) throw err;
+            db.collection(col_name).findOne({_id:mongo.ObjectId(req.body._id)},
+            (err,result) => {
+                res.send(`${result.name} is deactivated now`) 
+            })
+        }
+    )
+})
+
+// Activate
+app.put('/activate', (req,res) => {
+    db.collection(col_name).updateOne(
+        {_id:mongo.ObjectId(req.body._id)},
+        {
+            $set:{
+                isActive:true
+            }
+        },(err,result)=>{
+            if(err) throw err;
+            db.collection(col_name).findOne({_id:mongo.ObjectId(req.body._id)},
+            (err,result) => {
+                res.send(`${result.name} is Activated now`) 
+            })
+        }
+    )
 })
 
 
